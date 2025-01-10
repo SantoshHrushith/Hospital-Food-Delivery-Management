4a4c3518817cbaa77668a92bf70d9a0ab362af5f
SantoshHrushith <hrushith2004@gmail.com>
2025-01-10 20:57:10 +0530

    i

--- backend/index.js
+++ backend/index.js
@@ -9,37 +9,34 @@ import pantryRoutes from './routes/pantryRoutes.js'
 import deliveryRoutes from './routes/deliveryRoutes.js'
 import dashBoardRoutes from './routes/dashBoardRoutes.js'
 
+
+
 dotenv.config(); // Load .env file
 
 connectDB(); // Connect to MongoDB
-console.log('MONGO_URI:', process.env.MONGO_URI);  // Check if MONGO_URI is correct
-
+console.log('MONGO_URI:', process.env.MONGO_URI);
 const app = express();
-
-// CORS configuration (make sure the front-end URL matches the deployed URL)
-const corsOptions = {
+app.use(express.json());
+app.use(cors({
     origin: [
-        "http://localhost:3000",  // For local development
-        "https://hospital-food-delivery-management-f.vercel.app"  // Deployed front-end URL
+        "http://localhost:3000", // For local development
+        "https://hospital-food-delivery-management-f.vercel.app/" // Deployed front-end URL
     ],
-    methods: ["GET", "POST", "PUT", "DELETE"],  // Allowed methods
-    credentials: true,  // Allows cookies to be sent
-};
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true
+}));
 
-// Apply CORS middleware
-app.use(cors(corsOptions));
-
-app.use(express.json());  // To parse JSON in requests
 
 const PORT = process.env.PORT || 5000;
-app.options("*", cors());  // Allow pre-flight requests
+app.options("*", cors());
 
-// Routes for different API endpoints
 app.use('/api/users', userRoutes);
 app.use('/api/patients', patientRoutes);
 app.use('/api/diets', dietRoutes);
 app.use('/api/staff', pantryRoutes);
 app.use('/api/deliveries', deliveryRoutes);
 app.use('/api/dashboard', dashBoardRoutes);
+
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
