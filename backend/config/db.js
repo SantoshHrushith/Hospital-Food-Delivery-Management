edc1545732b9f94c89cd3d617eb06008b20c6fcb
SantoshHrushith <hrushith2004@gmail.com>
2025-01-10 20:15:48 +0530

    c

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect('mongodb+srv://Hrushith:VzzFGAQf3WKKHZno@cluster0.g6xpdzs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log('MongoDB Connected...');
     } catch (error) {
         console.error(`Error: ${error.message}`);
